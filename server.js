75be8c964bec90d75d7ab87d2de847dfd9b4d6eb
HaidarKhalid <HaidarKhalid01@gmail.com>
2024-07-18 17:59:27 +0300

    modify

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require("express")
+const examRouter = require("./routes/examRoutes.js")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/exams", examRouter)
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("connected"))
+.catch((error)=>console.log(error))
+
+const port = process.env.PORT || 8000;
+app.listen(port,()=> console.log("listening at port:" + port))
+
--- src/routes/examRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const { createExam, getAllExams, updateExam, getSingleExam, deleteExam } = require("../controllers/examControllers")
-const router = express.Router()
-
-router.route("/").get(getAllExams).post(createExam)
-router.route("/:examId").get(getSingleExam).put(updateExam).delete(deleteExam)
-
-module.exports = router
